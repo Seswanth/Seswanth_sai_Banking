9cac3d66c7109376a1ef639e6c4434d99975fe32
seswanth <seswanthsai2@gmail.com>
2026-05-19 04:32:17 +0000

    All done

--- HospitalManagement/src/Demo1.java
+++ HospitalManagement/src/Demo1.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Welcome to java programming");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your name");
+		String name = sc.nextLine();
+		System.out.println("Welcome " + name);
+		
+		
 	}
 
 }
